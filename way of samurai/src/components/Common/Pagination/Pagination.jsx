b1894d2e18b01b9fcbb81b27f0a09447a1f10dde
Harbor Store <harborstore@MacBook-Air-Harbor.local>
2022-05-20 21:29:58 +0300

    ended 93 lesson

--- way of samurai/src/components/Common/Pagination/Pagination.jsx
+++ way of samurai/src/components/Common/Pagination/Pagination.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "../../Users/Users.module.css";
 
 const Pagination = (props) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pagesCount = Math.ceil(props.totalItemsCount / props.pageSize);
 
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
+
+    let portionCount = Math.ceil(pagesCount/props.portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber-1)*props.portionSize+1;
+    let rightPortionPageNumber = portionNumber * props.portionSize;
+
     return <div>
-        {pages.map(p => {
+        <button onClick={() => setPortionNumber(1)}>First</button>
+        {portionNumber > 1 && <button onClick={() => setPortionNumber(portionNumber-1)}>Prev</button>}
+        {pages.filter(p => p>=leftPortionPageNumber && p<=rightPortionPageNumber)
+            .map(p => {
             return <span className={props.currentPage === p && styles.selectedPage}
                          onClick={() => {
                              props.onPageChanged(p)
                          }}>{p + ' '}</span>
         })}
+        {portionNumber < portionCount && <button onClick={() => setPortionNumber(portionNumber+1)}>Next</button>}
+        <button onClick={() => setPortionNumber(portionCount)}>Last</button>
     </div>
 }
 
--- way of samurai/src/components/Users/Users.jsx
+++ way of samurai/src/components/Users/Users.jsx
@@ -5,7 +5,8 @@ import User from "./User";
 const Users = (props) => {
     return <div>
     <Pagination currentPage={props.currentPage} onPageChanged={props.onPageChanged}
-                totalUsersCount={props.totalUsersCount} pageSize={props.pageSize}/>
+                totalItemsCount={props.totalUsersCount} pageSize={props.pageSize}
+                portionSize={20}/>
         {
             props.users.map(u => <User key={u.id}
                                        user={u}
